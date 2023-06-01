4c53d9f88b534bc4b26c9fcb59509d00e78170ef
JosephKabemba <josephkabemba99@gmail.com>
2023-06-01 13:28:19 +0100

    feat: create the portfolio card component

---
+++ app/portfolio/PortfolioCard/PortfolioCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+interface ICoverImage {
+  desktop?: string;
+  tablet?: string;
+  mobile?: string;
+}
+
+interface IPortfolioCard {
+  title: string;
+  date: string;
+  image: ICoverImage;
+}
+
+const PortfolioCard = ({ title, date, image }: IPortfolioCard) => {
+  return (
+    <article className="relative">
+      <picture>
+        <source media="(min-width:1024px)" srcSet={image.desktop} />
+        <source media="(min-width:768px)" srcSet={image.tablet} />
+        <img src={image.mobile} alt="portfolio" />
+      </picture>
+      <div className="absolute bottom-8 left-6">
+        <h2 className="font-bold text-white text-[32px] leading-10">{title}</h2>
+        <span className="font-medium text-white/80 text-[18px] leading-6">
+          {date}
+        </span>
+      </div>
+    </article>
+  );
+};
+
+export default PortfolioCard;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import PortfolioCard from "./PortfolioCard/PortfolioCard";
+
 const Portfolio = () => {
   return (
     <main>
       <h1>Portfolio Page</h1>
+      <section>
+        <PortfolioCard
+          title="Eebox Building"
+          date="August 2021"
+          image={{ desktop: "", tablet: "", mobile: "" }}
+        />
+      </section>
     </main>
   );
 };
